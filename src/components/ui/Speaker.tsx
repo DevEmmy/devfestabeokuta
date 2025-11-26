c44d8686501445ed11984953a1d2d7680b671218
frontend-starix <frontend@starixapp.com>
2025-11-26 21:56:09 +0100

    speakers info added

--- src/components/ui/Speaker.tsx
+++ src/components/ui/Speaker.tsx
@@ -12,80 +12,133 @@ const Speaker = () => {
 
     const speakerCards = [
         {
-            image: "speaker1.jpg",
-            name: "Olaosebikan emmy",
-            job: "gdg lead",
-            program: "Innovation Hub",
-            background: "bg-[#FF7DAF]",
-            desc: "A space where brilliant minds come together to create, learn, and build the future."
-        },
-        {
-            image: "speaker2.jpg",
-            name: "Olagoke Oyetunji",
-            job: "gdg lead",
-            program: "Tech Conference",
-            background: "bg-[#FFD427]",
-            desc: "Experience the energy of technology, collaboration, and groundbreaking ideas."
-        },
-        {
-            image: "speaker3.jpg",
-            name: "Abolade ilerioluwa",
-            job: "gdg lead",
-            program: "Developer Meetup",
-
+            image: "headshot1.jpg",
+            name: "Oluwole Majiyagbe",
+            job: "FullStack Developer, Monitaur.AI",
+            program: "GraphQL Development",
             background: "bg-[#CCF6C5]",
-            desc: "Connect with passionate developers, share insights, and grow together."
+            desc: "GraphQL for Beginners: Asking for What You Need"
+        },
+        {
+            image: "headshot2.jpg",
+            name: "Uchenna Nnamadim",
+            job: "Senior QA Engineer, MedMe Health",
+            program: "Quality Assurance & Testing",
+            background: "bg-[#A0E7E5]",
+            desc: "Taming Flaky Tests: From Headaches to Confidence"
+        },
+        {
+            image: "headshot3.jpg",
+            name: "Rufai Kudus",
+            job: "Senior Software Engineer, Sterling Bank Ng",
+            program: "Search Technologies",
+            background: "bg-[#CCF6C5]",
+            desc: "Vector-Powered Search: Building Intelligent Systems That Actually Understand Users"
+        },
+        {
+            image: "headshot4.jpg",
+            name: "Samuel Chinedu",
+            job: "Growth and Digital Marketing Specialist",
+            program: "Marketing Automation",
+            background: "bg-[#FFD427]",
+            desc: "The Accidental Engineer: What Marketing Automation Taught Me About Building Scalable Systems."
+        },
+        {
+            image: "headshot5.jpg",
+            name: "Adedoyinsola Adeyeye",
+            job: "Product Manager, Moniepoint",
+            program: "Product Management & Cloud Strategy",
+            background: "bg-[#CCF6C5]",
+            desc: "Beyond Features: How Systems Thinking and Cloud Strategy Are Redefining Product Management"
+        },
+        {
+            image: "headshot6.jpg",
+            name: "Adeshina Lasisi",
+            job: "Senior Backend Engineer, Nomba Financial Services",
+            program: "Distributed Systems",
+            background: "bg-[#FFD427]",
+            desc: "Building Distributed Systems with gRPC"
+        },
+        {
+            image: "headshot7.jpg",
+            name: "Cindy Shontan",
+            job: "Senior Product Designer",
+            program: "AI & UX Design",
+            background: "bg-[#F7D6E0]",
+            desc: "Designing for the Age of Agents: Creating Trustworthy Experiences with AI- Powered Interfaces"
+        },
+        {
+            image: "headshot8.jpg",
+            name: "Jesimiel Williams",
+            job: "Creative and Visual Director, Stutern",
+            program: "Product Development & Storytelling",
+            background: "bg-[#F7D6E0]",
+            desc: "Humanizing Code: How Storytelling can Build Products People Use"
+        },
+        {
+            image: "headshot9.jpg",
+            name: "Mileke Kolawole",
+            job: "Cloud Engineer",
+            program: "Serverless Architecture",
+            background: "bg-[#A0E7E5]",
+            desc: "Serverless for Startups: Accelerating Time-to-Market with GCP"
+        },
+        {
+            image: "headshot10.jpg",
+            name: "Bashir Hamza",
+            job: "Senior Software Engineer",
+            program: "AI Development Tools",
+            background: "bg-[#F7D6E0]",
+            desc: "Giving Your AI Eyes: Bringing the Power of Chrome DevTools to Your Coding Assistant using MCP."
         }
     ];
 
     return (
         <section className='bg-[#f0f0f0] min-h-screen pt-20 relative' id='speakers'>
-            <div className='  general-space flex flex-col gap-36  rounded-t-[10rem]'>
+            <div className='  general-space flex flex-col  rounded-t-[10rem]'>
 
                 <DoodleBg color='#1e1e1e' opacity={0.06} />
 
                 <TitleHead title='Speaker' />
 
 
-                <ComingSoonMask>
-                    <div className='grid grid-cols-1 md:grid-cols-3 md:gap-10 max-md:gap-20 mb-9'>
-                        {
-                            speakerCards.map((item: any, i: number) => (
-                                <div className={`${item.background} p-3 max-md:mb-7 border-3 border-[#1e1e1e] rounded-lg relative ${i > 0 && 'max-md:hidden'}`} key={i}>
-                                    <Image src={`/${item?.image}`} alt='item.name' width={1000} height={1000} className='w-full h-fit border-3 border-[#1e1e1e] rounded-sm' />
+                <div className="flex overflow-x-visible overflow-y-hidden max-md:py-5 gap-5 md:gap-10 pb-16 w-full -mt-10">
+                    {speakerCards.map((item: any, i: number) => (
+                        <div
+                            className={`${item.background} max-md:h-fit p-3 border-3 border-[#1e1e1e] rounded-lg relative max-md:min-w-[80vw] max-md:w-full max-md:max-w-full md:min-w-[400px] md:max-w-sms`}
+                            key={i}
+                        >
+                            <Image
+                                src={`/${item?.image}`}
+                                alt={item?.name}
+                                width={1000}
+                                height={1000}
+                                className="w-full min-h-[300px] max-h-[301px] md:min-h-[400px] md:max-h-[401px] object-cover object-top border-3 border-[#1e1e1e] rounded-sm"
+                            />
+
+                            <div className="flex justify-between">
+                                <div className="bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e]" />
+                                <div className="bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e]" />
+                            </div>
 
 
-                                    <div className="flex justify-between">
-                                        <div className='bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e]' />
 
-                                        <div className='bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e]' />
-                                    </div>
+                            <div className="bg-[#f0f0f0] p-3 shadow flex flex-col gap-3 rounded-lg border-3 border-[#1e1e1e]">
+                                <h3 className="text-2xl font-semibold">{item?.name}</h3>
+                                <em className="text-right text-[#444] text-sm font-medium line-clamp-2 ">~ {item?.job}</em>
 
-                                    <div className='bg-[#f0f0f0] rounded-lg border-3 border-[#1e1e1e] font-semibold w-fit p-3 absolute -top-10 translate-x-1/3 '>
-                                        {item?.name}
-                                        <div className='text-right text-[#444] text-sm font-medium'>
-                                            ~ {item?.job}
-                                        </div>
-                                    </div>
+                                <p className='pt-3 font-mono text-sm'> <span className='font-bold'>
+                                    TITLE: </span> {item?.desc}</p>
+                            </div>
 
-                                    <div className='bg-[#f0f0f0] p-3 shadow flex flex-col gap-3 rounded-lg border-3 border-[#1e1e1e]'>
-                                        <h3 className='text-2xl font-semibold'>{item?.program}</h3>
-                                        <p>{item?.desc}</p>
-                                    </div>
+                            {/* <div className="bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -top-5 -left-3" />
+                            <div className="bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -top-5 -right-3" />
+                            <div className="bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -bottom-5 -left-3" />
+                            <div className="bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -bottom-5 -right-3" /> */}
+                        </div>
+                    ))}
+                </div>
 
-
-                                    <div className='bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -top-5 -left-3' />
-
-                                    <div className='bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -top-5 -right-3' />
-
-                                    <div className='bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -bottom-5 -left-3' />
-
-                                    <div className='bg-[#f0f0f0] size-5 border-2 border-[#1e1e1e] absolute -bottom-5 -right-3' />
-                                </div>
-                            ))
-                        }
-                    </div>
-                </ComingSoonMask>
 
 
 
